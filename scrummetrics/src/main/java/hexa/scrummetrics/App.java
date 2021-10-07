2c9e3eeed6a4e505bb1b59ddfa8e85d13014a8a8
Jonatan Reynosa <jonatan.picls@hotmail.com>
2021-10-07 15:38:55 -0500

    First Commit

---
+++ scrummetrics/src/main/java/hexa/scrummetrics/App.java
@@ -0,0 +1,13 @@
+package hexa.scrummetrics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
