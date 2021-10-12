fc968d6b5b8fa67219e2d41196805418db811832
Angel Del Río <AngelD1@hexaware.com>
2021-10-12 12:23:12 -0500

    Added user test for Login

---
+++ scrummetrics/src/main/java/pages/Login.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login extends Base{
+
+	public Login(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+	By usernameLocator = By.id("mat-input-0");
+	By passLocator = By.id("mat-input-1");
+	By loginLocator = By.xpath("//button[@class='mat-raised-button']");
+	
+	public void fillLogin(String username, String password) {
+		type(username, usernameLocator);
+		type(password, passLocator);
+		submit(loginLocator);
+	}
+	
+	public String wrongUser() {
+		waitAlert();
+		return getMessage();
+	}
+	
+}
