59453f4b1214135b08fed83f0ce96316e9c44388
Angel Del Río <AngelD1@hexaware.com>
2021-10-13 11:44:56 -0500

    Removed unnecessary templates

--- scrummetrics/src/main/java/pages/UserData.java
+++
@@ -1,7 +0,0 @@
-package pages;
-
-public class UserData {
-	public String fullData(String a, String b, String c, String d, String e) {
-		return String.format("%s %s %s %s %s", a, b, c, d, e);
-	}
-}
